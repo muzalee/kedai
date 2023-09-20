494189eee0aa8b8f8fa4bae3bb10bde1f5a477fb
ainnaziha <ainnaziha551@gmail.com>
2023-09-21 00:24:53 +0800

    fix: include category in product card hihi

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,9 +1,12 @@
+import { Category } from "./category.model";
+
 export class Product {
   id: number;
   name: string;
   description: string;
   image: string;
   formattedPrice: string;
+  category: Category;
   isLoading?: boolean;
 
   constructor(data: any) {
@@ -12,5 +15,7 @@ export class Product {
     this.description = data.description;
     this.image = `assets/${data.image}`;
     this.formattedPrice = data.formattedPrice;
+    this.category = new Category(data.category);
+
   }
 }
