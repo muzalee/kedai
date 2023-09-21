531b6cec6059e928dea7b4ef1a763010c643ef42
ainnaziha <ainnaziha551@gmail.com>
2023-09-22 01:32:53 +0800

    feat: add order model

---
+++ src/app/models/order.model.ts
@@ -0,0 +1,13 @@
+import { Cart } from "./cart.model";
+
+export class Order {
+  orderNo: string;
+  total: string;
+  carts: Cart[];
+
+  constructor(data: any) {
+    this.orderNo = data.orderNo,
+    this.total = data.total ?? '0.00';
+    this.carts = data.carts == null ? [] : data.items.map((item) => new Cart(item));
+  }
+}
